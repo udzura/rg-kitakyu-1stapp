4f237616c4d7ed0a08ff547022a0f45673fe5d2f
Uchio KONDO <udzura@pepabo.com>
2018-07-25 18:08:27 +0900

    Add welcome

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  get '/rails/info/properties' => "rails/info#properties"
+  get '/rails/info/routes'     => "rails/info#routes"
+  get '/rails/info'            => "rails/info#index"
+  root to: "rails/welcome#index"
+
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
   resources :ideas
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
